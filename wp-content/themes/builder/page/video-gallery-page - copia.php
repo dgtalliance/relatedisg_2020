2d81b747079e7807f8144badb81c59cca8dfabb0
Oliver <oliver@dgtalliance.com>
2020-09-10 11:00:25 -0500

    add video gallery page

---
+++ wp-content/themes/builder/page/video-gallery-page - copia.php
@@ -0,0 +1,15 @@
+<?php 
+/*
+Template Name: Video Gallery
+*/
+get_header();
+while ( have_posts() ) : the_post(); ?>
+
+<main id="video-page">
+  <section class="ms-section ms-animate" id="ms-video-page">
+    <h2 class="ms-title">RELATEDISG <span>Website / CRM Video Tutorials</span></h2>
+    <?php echo do_shortcode('[lazy_videos]');?>
+  </section>
+</main>
+
+<?php endwhile; get_footer(); ?> 
