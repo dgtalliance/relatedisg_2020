ec9cda779b87628117d93bd783304110c4bcdcbd
Oliver <oliver@dgtalliance.com>
2020-09-17 11:29:41 -0500

    fix buttons to join agent page

--- wp-content/themes/builder/page/video-gallery-page.php
+++ wp-content/themes/builder/page/video-gallery-page.php
@@ -7,7 +7,7 @@ while ( have_posts() ) : the_post(); ?>
 
 <main id="video-page">
   <section class="ms-section ms-animate" id="ms-video-page">
-    <h2 class="ms-title">RELATEDISG <span>Website / CRM Video Tutorials</span></h2>
+    <h2 class="ms-title">RELATEDISG <span>Website / CRM Tutorials</span></h2>
     <?php echo do_shortcode('[lazy_videos]');?>
   </section>
 </main>
